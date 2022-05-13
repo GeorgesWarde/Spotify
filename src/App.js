4afe2399a922b66576ca045f4c476964c015b9ae
GeorgesWarde <warde-georges123@outlook.com>
2022-05-13 22:35:24 +0300

    Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
+import React,{useEffect,useState} from 'react';
 import './App.css';
+import Login from './Components/Login';
+import SearchArtists from './Components/searchArtists';
+
 
 function App() {
+  const  [albums , setAlbums] = useState([])
+  const [Token,setToken]=useState()
+  const getToken = () => {
+    return window.location.hash
+      .substring(1)
+      .split("&")
+      .reduce((initial, item) => {
+        var parts = item.split("=");
+        initial[parts[0]] = decodeURIComponent(parts[1]);
+  
+        return initial;
+      }, {});
+  };
+  
+  
+    
+useEffect(()=>{
+  localStorage.clear()
+  const token = getToken().access_token
+  
+  if(token){
+    setToken(token)
+    localStorage.setItem("myToken",Token)
+
+    console.log(Token)
+  }
+})
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {!Token && <Login/>}
+      {Token && <SearchArtists/>}
     </div>
-  );
+  )
 }
 
 export default App;
